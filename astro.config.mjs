0929363b9ba290b2195961f5fc685505a54ef4da
VVX Supp <supp@0day.is>
2026-05-04 12:16:58 +0900

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://haunted.fail",
+	site: "https://blogue.haunted.fail",
 	base: "/",
 	trailingSlash: "always",
 	i18n: {
